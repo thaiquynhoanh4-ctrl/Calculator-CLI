e9dfea6388ca21a8fe99a972ae7448c11b6b04c2
thaiquynhoanh4-ctrl <thaiquynhoanh4@gmail.com>
2026-07-04 21:10:08 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from calculator import add, subtract, multiply, divide
+
+a = float(input("First number: "))
+b = float(input("Second number: "))
+
+print("\nResults")
+print("Add:", add(a, b))
+print("Subtract:", subtract(a, b))
+print("Multiply:", multiply(a, b))
+print("Divide:", divide(a, b))
